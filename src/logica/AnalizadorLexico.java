9ed6526d28465caebdd2df82e6e2f62cf0a37009
CDCSarjento <sebastian_cdc@hotmail.com>
2021-10-03 17:57:27 -0500

    funcional

--- src/logica/AnalizadorLexico.java
+++ src/logica/AnalizadorLexico.java
@@ -6,39 +6,38 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-
 public class AnalizadorLexico {
-    
+
     public static void main(String[] args) throws Exception {
-        
-        String caminoJFLEX= "../AnalizadorLexico_SC/src/logica/Lexico.flex";
-        String caminoJCUP= "../AnalizadorLexico_SC/src/logica/LexicoCup.flex";
-        String[] caminoSCUP= {"-parser","Sintax","../AnalizadorLexico_SC/src/logica/Sintax.cup"};
-        generarLex(caminoJFLEX, caminoJCUP, caminoSCUP);
+        final String rutaJFLEX = "src/logica/Lexico.flex";
+        final String rutaJCUP = "src/logica/LexicoCup.flex";
+        final String[] rutaSCUP = {"-parser", "Sintax", "src/logica/Sintax.cup"};
+        generarLex(rutaJFLEX, rutaJCUP, rutaSCUP);
     }
+
     public static void generarLex(String caminoJFLEX, String caminoJCUP, String[] caminoSCUP) throws IOException, Exception {
-        File arc; 
-        arc = new File(caminoJFLEX);
-        JFlex.Main.generate(arc);
-        arc = new File(caminoJCUP);
-        JFlex.Main.generate(arc);
-        java_cup.Main.main(caminoSCUP);     
-        
-        Path caminoSym = Paths.get("../AnalizadorLexico_SC/src/logica/sym.java");
+        File arc1, arc2;
+        arc1 = new File(caminoJFLEX);
+        JFlex.Main.generate(arc1);
+        arc2 = new File(caminoJCUP);
+        JFlex.Main.generate(arc2);
+        java_cup.Main.main(caminoSCUP);
+
+        Path caminoSym = Paths.get("src/logica/sym.java");
         if (Files.exists(caminoSym)) {
             Files.delete(caminoSym);
         }
         Files.move(
-                Paths.get("../AnalizadorLexico_SC/sym.java"), 
-                Paths.get("../AnalizadorLexico_SC/src/logica/sym.java")
+                Paths.get("src/logica/sym.java"),
+                Paths.get("src/logica/sym.java")
         );
-        Path caminoSint = Paths.get("../AnalizadorLexico_SC/src/logica/Sintax.java");
+        Path caminoSint = Paths.get("src/logica/Sintax.java");
         if (Files.exists(caminoSint)) {
             Files.delete(caminoSint);
         }
         Files.move(
-                Paths.get("../AnalizadorLexico_SC/Sintax.java"), 
-                Paths.get("../AnalizadorLexico_SC/src/logica/Sintax.java")
-        );        
+                Paths.get("src/logica/Sintax.java"),
+                Paths.get("src/logica/Sintax.java")
+        );
     }
 }
